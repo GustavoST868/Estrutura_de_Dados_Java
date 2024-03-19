e94ad620cd7c8b4ecf64fb027766bad814d85486
Fulano de Tal <gustavosantosteixeira98@gmail.com>
2024-03-19 17:06:21 -0300

    2

--- Estrutura de Dados/src/Lista/List.java
+++ Estrutura de Dados/src/Lista/List.java
@@ -1,5 +1,22 @@
 package Lista;
 
-public class List {
-    public List(){}
+public class List<T> {
+
+    private class Node{
+        T data;
+        Node next;
+        Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+    public List(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 }
