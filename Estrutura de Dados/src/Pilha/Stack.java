2a9ddaf263c5a126fed5ff61999266004029a9e4
Fulano de Tal <gustavosantosteixeira98@gmail.com>
2024-03-23 15:10:24 -0300

    adfasd

--- Estrutura de Dados/src/Pilha/Stack.java
+++ Estrutura de Dados/src/Pilha/Stack.java
@@ -1,7 +1,29 @@
 package Pilha;
 
 public class Stack {
-    public Stack(){
 
+    //attributes
+    private Node top;
+    private int size;
+    private int  indexCounter;
+
+    public Stack(){
+        this.top = null;
+        this.size = 0;
+        this.indexCounter = 0;
+    }
+
+    //class node to store the data
+    public class Node{
+        //attributes
+        private String data;
+        private Node next;
+        private int index;
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+            //add one more to each node created
+            this.index = indexCounter++;
+        }
     }
 }
